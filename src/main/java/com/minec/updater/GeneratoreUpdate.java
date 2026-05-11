b9f28f9d6cdc8021813b80b6b09ece430b03bc48
edobolo <bologniniedoardo@gmail.com>
2026-05-11 20:46:26 +0200

    second try updates

--- src/main/java/com/minec/updater/GeneratoreUpdate.java
+++ src/main/java/com/minec/updater/GeneratoreUpdate.java
@@ -1,4 +1,5 @@
 package com.minec.updater;
+
 import org.update4j.Configuration;
 import org.update4j.FileMetadata;
 import java.io.Writer;
@@ -8,24 +9,28 @@ import java.nio.file.Paths;
 public class GeneratoreUpdate {
     public static void main(String[] args) throws Exception {
 
-        // Costruiamo la mappa di aggiornamento
         Configuration config = Configuration.builder()
-                // 1. Dove saranno i file su internet? (Es. una repo GitHub Pages)
-                .baseUri("https://edobolo.github.io/uniplanner-updates")
+                // 1. IL LINK GIUSTO DOVE SONO I FILE
+                .baseUri("https://raw.githubusercontent.com/edobolo/UniPlanner/main/aggiornamenti")
 
-                // 2. Dove andranno salvati sul PC dell'utente? (nella cartella da cui avvia il
-                // programma)
+                // 2. DOVE SALVARLI SUL PC
                 .basePath("${user.dir}")
 
-                // 3. Quali file fanno parte dell'app? (Devi avere il tuo file .jar pronto)
-                .file(FileMetadata.readFrom("target/UniPlanner.jar").uri("UniPlanner.jar"))
+                // 3. IL FILE JAR (Aggiungiamo .classpath() per dire che è codice eseguibile)
+                .file(FileMetadata.readFrom("target/UniPlanner.jar")
+                        .uri("UniPlanner.jar")
+                        .classpath())
+
+                // 4. LA CLASSE DA AVVIARE (Sostituisci con il nome corretto!)
+                // Scrivi qui il nome completo della classe che apre il vero UniPlanner.
+                .property("default.launcher.main.class", "com.minec.MainApp")
+
                 .build();
 
-        // Salviamo la mappa in un file config.xml
         try (Writer out = Files.newBufferedWriter(Paths.get("config.xml"))) {
             config.write(out);
         }
 
-        System.out.println("File config.xml generato con successo! Pronto per l'upload.");
+        System.out.println("Nuovo config.xml generato con i parametri di avvio corretti!");
     }
 }
